9c67d5e2e7541b44d083d706ac2f109841e5d163
m.abdalgafour@gmail.com <m.abdalgafour@gmail.com>
2021-06-03 16:59:41 +0200

    add features

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@
 
  
 function App() {
+  const currentHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
+  const currentMinute = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
   return (
     <div className="App">
        <header className="App-header">
@@ -15,6 +17,16 @@ function App() {
        <div className={classees.MainContainer}>
          <div className={classees.ProductPreview}>
             <img src="https://imgur.com/iOeUBV7.png" alt="product Review" />
+
+            {/* <div className={classees.TimeSection}> 
+              <p>{currentHour}: {currentMinute}</p>
+            </div> */}
+
+            <div className={classees.HeartBeataSection}> 
+              <i className="fas fa-heartbeat"></i>
+              <p>78</p>
+            </div>
+
          </div>
           <div className={classees.ProductData}>
               <h1 className={classees.ProductTitle}>{ProductData.title}</h1>
@@ -27,6 +39,13 @@ function App() {
                 <img className={classees.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue Color" />
                 <img className={classees.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Perbule Color" />
                </div>
+
+               <h3 className={classees.SectionHeading}>Featured</h3>
+               <div>
+                 <button className={[classees.FeatureItem , classees.SelectedFeatureItem].join(' ')}>Time</button>
+                 <button className={classees.FeatureItem}>Heart Rate</button>
+               </div>
+               <button className={classees.PrimatyBtn}>Buy Now</button>
           </div>       
        
        </div>
